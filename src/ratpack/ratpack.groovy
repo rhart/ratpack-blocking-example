a5187c55a59a2912d737707755823fbf7f97bab4
rhart <russellxhart@googlemail.com>
2014-05-17 21:16:40 +0100

    Upgrade to 0.9.4

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -7,16 +7,16 @@ ratpack {
             println "${new Date()} - received blocking request"
             Thread.sleep(10000)
             println "${new Date()} - replying blocking request"
-            response.send()
+            render ""
         }
 
         get("non-blocking") {
             println "${new Date()} - received non-blocking request"
-            background {
+            blocking {
                 Thread.sleep(10000)
             } then{
                 println "${new Date()} - replying non-blocking request"
-                response.send()
+                render ""
             }
         }
 
